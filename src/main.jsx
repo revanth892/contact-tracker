0dc75ffd4737ea8d719925eda61ee485d61aef8b
revanth892 <55280048+revanth892@users.noreply.github.com>
2023-02-22 19:43:13 +0530

    adding root element

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Root from "./routes/root";
+import {createBrowserRouter,RouterProvider,createRoutesFromElements,Route} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <p>this is main page</p>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
